6e36c975d87c798021a3fc7cbe277afee7cfc01b
Darling91 <87092064+Darling91@users.noreply.github.com>
2023-05-30 21:42:51 +0300

    Add files via upload

---
+++ pepethefrog/webapp/controller/Object.controller.js
@@ -0,0 +1,271 @@
+sap.ui.define([
+		"zjblessons/pepethefrog/controller/BaseController",
+		"sap/ui/model/json/JSONModel",
+		"sap/ui/core/routing/History",
+		"zjblessons/pepethefrog/model/formatter",
+		"sap/ui/model/Sorter",
+		"sap/ui/model/Filter",
+		"sap/ui/model/FilterOperator",
+		"sap/m/MessageBox",
+		"sap/m/MessageToast",
+		"sap/ui/core/Fragment"
+	], function (
+		BaseController,
+		JSONModel,
+		History,
+		formatter,
+		Sorter,
+		Filter,
+		FilterOperator,
+		MessageBox,
+		MessageToast,
+		Fragment,) {
+		"use strict";
+
+		return BaseController.extend("zjblessons.pepethefrog.controller.Object", {
+
+			formatter: formatter,
+
+			onInit : function () {
+				this.getRouter().getRoute("object").attachPatternMatched(this._onObjectMatched, this);
+
+				
+				var iOriginalBusyDelay,
+					oViewModel = new JSONModel({
+						busy : true,
+						delay : 0,
+						editMode :false,
+						selectedKeyITB: "list",
+					});
+					this.setModel(oViewModel, "objectView");
+
+				
+				
+				iOriginalBusyDelay = this.getView().getBusyIndicatorDelay();
+				
+				this.getOwnerComponent().getModel().metadataLoaded().then(function () {
+						
+						oViewModel.setProperty("/delay", iOriginalBusyDelay);
+					}
+				);
+			},
+
+			
+			onNavBack : function() {
+				var sPreviousHash = History.getInstance().getPreviousHash();
+
+				if (sPreviousHash !== undefined) {
+					history.go(-1);
+				} else {
+					this.getRouter().navTo("worklist", {}, true);
+				}
+			},
+
+			
+			_onObjectMatched : function (oEvent) {
+				const sObjectId =  oEvent.getParameter("arguments").objectId;
+				this.getModel().metadataLoaded().then( function() {
+					const sObjectPath = this.getModel().createKey("/zjblessons_base_Materials", {
+						MaterialID :  sObjectId
+					});
+					this._bindView(sObjectPath);
+				}.bind(this));
+			},
+
+			
+			_bindView : function (sObjectPath) {
+				const oViewModel = this.getModel("objectView"),
+					oDataModel = this.getModel();
+
+				this.getView().bindElement({
+					path: sObjectPath,
+					events: {
+						change: this._onBindingChange.bind(this),
+						dataRequested: function () {
+							oDataModel.metadataLoaded().then(function () {
+								
+								oViewModel.setProperty("/busy", true);
+							});
+						},
+						dataReceived: function () {
+							oViewModel.setProperty("/busy", false);
+						}
+					}
+				});
+			},
+
+			_onBindingChange : function () {
+				var oView = this.getView(),
+					oViewModel = this.getModel("objectView"),
+					oElementBinding = oView.getElementBinding();
+				if (!oElementBinding.getBoundContext()) {
+					this.getRouter().getTargets().display("objectNotFound");
+					return;
+				}
+
+				let oResourceBundle = this.getResourceBundle(),
+					oObject = oView.getBindingContext().getObject(),
+					sObjectId = oObject.MaterialID,
+					sObjectName = oObject.MaterialText;
+
+				oViewModel.setProperty("/busy", false);
+
+				oViewModel.setProperty("/shareSendEmailSubject",
+				oResourceBundle.getText("shareSendEmailObjectSubject", [sObjectId]));
+				oViewModel.setProperty("/shareSendEmailMessage",
+				oResourceBundle.getText("shareSendEmailObjectMessage", [sObjectName, sObjectId, location.href]));
+			},
+
+			onPressEditMaterial: function(){
+				this._setEditMode(true);
+			},
+
+			onPressSaveMaterial: function(){
+				this.getModel().submitChanges();
+				this._setEditMode(false);
+			},
+
+			onPressCancelMaterial: function(){
+				this.getModel().resetChanges();
+				this._setEditMode(false);
+			},
+
+			
+
+			onChange: function(oEvent){
+				const bState=oEvent.getParameter('state');
+				if(!bState && this.getModel().hasPendingChanges()){
+					sap.m.MessageBox.confirm(this.getResourceBundle().getText("msgSaveChanges"),{
+						title: this.getResourceBundle().getText("ttlConfirmAction"),
+						actions:[
+							sap.m.MessageBox.Action.OK,
+							sap.m.MessageBox.Action.CANCEL,
+							sap.m.MessageBox.Action.CLOSE,
+						],
+						emphasizedAction: sap.m.MessageBox.Action.OK,
+						initialFocus: null,
+						textDirection: sap.ui.core.TextDirection.Inherit,
+						onClose: function(sAction){
+							if(sAction === "OK"){
+								this.onPressSaveMaterial();
+							}else if(sAction === "CANCEL"){
+								this.onPressCancelMaterial();
+							}
+						}.bind(this),
+					});
+					this._setEditMode(true);
+				}else{
+					this._setEditMode(bState);
+				}
+			},
+
+			_setEditMode: function(bMode){
+				this.getModel("objectView").setProperty("/editMode", bMode);
+				const sSelectedKey = this.getModel("objectView").getProperty("/selectedKeyITB");
+				if(bMode && sSelectedKey ==="list"){
+					this._bindGroupSelect();
+					this._bindSubGroupSelect();
+				}else if(sSelectedKey === "form"){
+					this._addFormContent(bMode ? "Edit" : "View");
+				}
+			}, 
+
+			_bindGroupSelect: function(){
+				this.byId("groupSelect").bindItems({
+					path:"/zjblessons_base_Groups",
+					template: new sap.ui.core.Item({
+						key: "{GroupID}",
+						text: "{GroupText}"
+					}),
+					sorter: new Sorter("GroupText",false),
+					filters: new Filter("GroupText", FilterOperator.NE, null)
+				})
+			},
+
+			_bindSubGroupSelect: function(){
+				this._getSubGroupSelect().then((oTemplate) => {
+					this.byId("subGroupSelect").bindAggregation("items", {
+						path:'/zjblessons_base_SubGroups',
+						template: oTemplate,
+						sorter: 
+						[new Sorter("SubGroupText",false), 
+						 new Sorter({ path: 'Created', descending: true}),
+						],
+						filters: 
+						[new Filter("SubGroupText",FilterOperator.NE, null),
+						new Filter({path:'Created',operator: FilterOperator.BT, 
+						value1:new Date("2023-03-11"),value2: new Date(),})
+						],
+						events:{
+							dataRequested:() => {
+								MessageToast.show("Requesting SubGroups...")
+							},
+							dataReceived: (oData) => {
+								MessageBox.show(this._getMessageSubGroupsLoaded(oData),{
+									title:this.getResourceBundle().getText("lSubGroupText"),
+								})
+							}
+						},
+					});
+				}).catch(() => {
+					this._setEditMode(false);
+				});
+			},
+
+			_getMessageSubGroupsLoaded: function(oData){
+				let sSubGroupText = "";
+				oData.mParameters.data.results.forEach((oSubGroup) =>{
+					sSubGroupText += oSubGroup.SubGroupText + ", "
+				})
+				return `${this.getResourceBundle().getText("msgGotSubGroups")} ${sSubGroupText.slice(0,sSubGroupText.length -2)}`;
+			},
+
+			_getSubGroupSelect: function(){
+				return new Promise((resolve, reject) =>{
+					if (!this._pSubGroupSelect){
+						this._pSubGroupSelect = Fragment.load({
+							id: this.getView().getId(),
+							name: "zjblessons.pepethefrog.view.fragment.SubGroupSelect",
+							controller: this,
+						}).then((oTemplate) => oTemplate);
+					}
+					this._pSubGroupSelect.then((oTemplate) => {
+						resolve(oTemplate);
+					}).catch((oError) => {
+						MessageBox.error(oError.toString());
+						reject();
+					});
+				});
+			},
+
+			onSelectIconTabBar: function(oEvent) {
+				const sSelectedKey = oEvent.getSource().getSelectedKey();
+				this.getModel("objectView").setProperty("/selectedKeyITB", sSelectedKey);
+				if(sSelectedKey !== "form") return;
+
+				this._addFormContent("View");
+				
+			},
+
+			_addFormContent: function(sMode){
+				if(!this[`_pFrom${sMode}`]){
+					this[`_pFrom${sMode}`] = Fragment.load({
+							id: this.getView().getId(),
+							name: "zjblessons.pepethefrog.view.fragment.Form" + sMode,
+							controller: this,
+						}).then((oContent) => {
+							this.getView().addDependent(oContent);
+							return oContent;
+						});
+					}
+					this[`_pFrom${sMode}`].then((oContent) => {
+						const oIconTabFilter = this.byId("formIconTabFilter");
+
+						oIconTabFilter.removeAllContent();
+						oIconTabFilter.insertContent(oContent, 0);
+					})
+
+				},
+			});
+		}
+);
---
+++ pepethefrog/webapp/controller/Worklist.controller.js
@@ -0,0 +1,185 @@
+sap.ui.define([
+		"zjblessons/pepethefrog/controller/BaseController",
+		"sap/ui/model/json/JSONModel",
+		"zjblessons/pepethefrog/model/formatter",
+		"sap/ui/model/Filter",
+		"sap/ui/model/FilterOperator",
+		"sap/ui/core/Fragment"
+	], function (BaseController, JSONModel, formatter, Filter, FilterOperator,Fragment) {
+		"use strict";
+
+		return BaseController.extend("zjblessons.pepethefrog.controller.Worklist", {
+
+			formatter: formatter,
+
+
+			onInit : function () {
+				var oViewModel,
+					iOriginalBusyDelay,
+					oTable = this.byId("table");
+
+
+				iOriginalBusyDelay = oTable.getBusyIndicatorDelay();
+
+				this._aTableSearchState = [];
+
+
+				oViewModel = new JSONModel({
+					worklistTableTitle : this.getResourceBundle().getText("worklistTableTitle"),
+					shareOnJamTitle: this.getResourceBundle().getText("worklistTitle"),
+					shareSendEmailSubject: this.getResourceBundle().getText("shareSendEmailWorklistSubject"),
+					shareSendEmailMessage: this.getResourceBundle().getText("shareSendEmailWorklistMessage", [location.href]),
+					tableNoDataText : this.getResourceBundle().getText("tableNoDataText"),
+					tableBusyDelay : 0
+				});
+				this.setModel(oViewModel, "worklistView");
+
+
+				oTable.attachEventOnce("updateFinished", function(){
+
+					oViewModel.setProperty("/tableBusyDelay", iOriginalBusyDelay);
+				});
+			},
+
+
+			onUpdateFinished : function (oEvent) {
+
+				var sTitle,
+					oTable = oEvent.getSource(),
+					iTotalItems = oEvent.getParameter("total");
+
+				if (iTotalItems && oTable.getBinding("items").isLengthFinal()) {
+					sTitle = this.getResourceBundle().getText("worklistTableTitleCount", [iTotalItems]);
+				} else {
+					sTitle = this.getResourceBundle().getText("worklistTableTitle");
+				}
+				this.getModel("worklistView").setProperty("/worklistTableTitle", sTitle);
+			},
+			
+			_showObject: function (oItem) {
+				this.getRouter().navTo("object", {
+					objectId: oItem.getBindingContext().getProperty("MaterialID")
+				});
+			},
+
+			onPressShowMaterial : function (oEvent) {
+
+				this._showObject(oEvent.getSource());
+			},
+
+
+			onNavBack : function() {
+				history.go(-1);
+			},
+
+	onShareInJamPress: function () {
+			var oViewModel = this.getModel("worklistView"),
+				oShareDialog = sap.ui.getCore().createComponent({
+					name: "sap.collaboration.components.fiori.sharing.dialog",
+					settings: {
+						object: {
+							id: location.href,
+							share: oViewModel.getProperty("/shareOnJamTitle")
+						}
+					}
+				});
+			oShareDialog.open();
+		},
+
+		_loadCreateFragment: function(oEntryContext){
+			if(!this.oCreateDialog){
+				this.pCreateMaterial = Fragment.load({
+					name:"zjblessons.pepethefrog.view.fragment.CreateMaterial",
+					controller: this,
+					id: "fCreateDialog"
+				}).then(oDialog => {
+					this.oCreateDialog = oDialog;
+					this.getView().addDependent(this.oCreateDialog);
+					return Promise.resolve(oDialog);
+				});
+			}
+			this.pCreateMaterial.then(oDialog => {
+				oDialog.setBindingContext(oEntryContext);
+				oDialog.open();
+			})
+		},
+
+		_closeCreateDialog:function(){
+			this.oCreateDialog.close();
+		},
+
+		onPressSaveMaterial: function(){
+			this.getModel().submitChanges();
+			this._closeCreateDialog();
+		},
+
+		onPressCloseCreateDialog:function(){
+			this.getModel().resetChanges();
+			this._closeCreateDialog();
+		},
+
+		onPressCreateMaterial: function(){
+			const mProperties = {
+				MaterialID: "0",
+				Version: "A",
+				Language: "RU"
+			};
+			const oEntryContext = this.getModel().createEntry("/zjblessons_base_Materials",{
+				properties: mProperties
+			});
+			this._loadCreateFragment(oEntryContext);
+		},
+
+		onPressDeleteMaterial: function(oEvent){
+			const sEntryPath = oEvent.getSource().getBindingContext().getPath();
+			this.getModel().remove(sEntryPath);
+		},
+
+		onSearch: function (oEvent) {
+				const aFilters = [];
+				const sValue = oEvent.getParameter("query") || oEvent.getParameter("newValue"); 	
+				if (sValue) {
+					aFilters.push(
+						new Filter({
+							filters: [
+							new Filter({
+								path:"MaterialText",
+								operator:FilterOperator.Contains,
+								value1: oEvent.getParameter("query") || oEvent.getParameter("newValue")
+							}),
+							new Filter("MaterialDescription",FilterOperator.Contains,sValue),
+							new Filter({
+								filters :[
+									new Filter(
+										"CreatedByFullName",FilterOperator.Contains,sValue
+									),
+									new Filter(
+										"ModifiedByFullName",FilterOperator.Contains,sValue
+									),
+								],
+								and: true,
+							})
+						],
+						and:false,
+						}),				
+					); 
+				}
+				this.byId("table").getBinding("items").filter(aFilters);
+		},
+
+		onPressRefresh: function(){
+			var oTable = this.byId("table");
+			oTable.getBinding("items").refresh();
+		},
+
+		_applySearch: function (aTableSearchState) {
+			var oTable = this.byId("table"),
+				oViewModel = this.getModel("worklistView");
+			oTable.getBinding("items").filter(aTableSearchState, "Application");
+			if (aTableSearchState.length !== 0) {
+				oViewModel.setProperty("/tableNoDataText", this.getResourceBundle().getText("worklistNoDataWithSearchText"));
+			}
+		}
+		});
+	}
+);
